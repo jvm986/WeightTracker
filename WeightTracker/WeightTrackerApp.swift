8e5694a4f2bd0037859b550344c7fb420b314ce0
James Maguire <jvm986@gmail.com>
2022-09-14 19:01:17 +0200

    Initial Commit

---
+++ WeightTracker/WeightTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightTrackerApp.swift
+//  WeightTracker
+//
+//  Created by James Maguire on 14/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeightTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
